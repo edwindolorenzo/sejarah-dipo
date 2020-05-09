21855a7be7e7ce73b70caf058ad7b4fbae125157
edwindolorenzo <47584233+edwindolorenzo@users.noreply.github.com>
2020-05-09 19:57:37 +0700

    add playercontroller
    Add player controller script

---
+++ Sripsi history/Assets/PlayerRideController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerRideController : PhysicsObject
+{
+    public float jumpTakeOffSpeed = 7;
+
+    protected override void ComputeVelocity()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            velocity.y = jumpTakeOffSpeed;
+        }
+    }
+}
