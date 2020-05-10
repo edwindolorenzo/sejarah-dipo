607ecc8b0deab6afedaf25ccdf5d65766a9a3057
edwindolorenzo <47584233+edwindolorenzo@users.noreply.github.com>
2020-05-10 11:46:20 +0700

    Update Scene
    Update Scene Lv 1 and Main Menu

--- Sripsi history/Assets/CameraScript.cs
+++ Sripsi history/Assets/CameraScript.cs
@@ -9,22 +9,12 @@ public class CameraScript : MonoBehaviour
     void Start()
     {
         // screen in y size
-        //float screenRatio = (float)Screen.width / (float)Screen.height;
-        //float targetRatio = rink.bounds.size.x* Screen.height / Screen.width * 0.5f;
-
-
-        //if(screenRatio >= targetRatio)
-        //Camera.main.orthographicSize = rink.bounds.size.y/2;
-        //else
-        //{
-        //    float differenceInSize = targetRatio / screenRatio;
-        //    Camera.main.orthographicSize = rink.bounds.size.y / 2 * differenceInSize;
-        //}
-
-        // screen in x size
-        float orthoSize = cameraSize.bounds.size.x * Screen.height / Screen.width * 0.5f;
-
+        float orthoSize = cameraSize.bounds.size.y / 2;
         Camera.main.orthographicSize = orthoSize;
+        // screen in x size
+        //float orthoSize = cameraSize.bounds.size.x * Screen.height / Screen.width * 0.5f;
+
+        //Camera.main.orthographicSize = orthoSize;
     }
 
 }
