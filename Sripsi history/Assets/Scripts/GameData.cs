f95caadbad09a6fc5185b091f22c6ac50a002313
edwindolorenzo <47584233+edwindolorenzo@users.noreply.github.com>
2020-05-21 16:59:42 +0700

    update save and load game
    Update Game Data script
    Update Game Manager Script
    Update MiniGame script

--- Sripsi history/Assets/Scripts/GameData.cs
+++ Sripsi history/Assets/Scripts/GameData.cs
@@ -9,9 +9,11 @@ public class GameData
     public bool[] levelClear;
     public int[] idChalange;
     public bool[] chalangeClear;
+    public string[] chalangeDescription;
     public float[] miniGameScore;
     public int[] idMiniGame;
     public bool[] openMiniGame;
+    public string[] miniGameDescription;
 
     public GameData(List<Stage> stages, List<MiniGame> miniGames)
     {
@@ -19,9 +21,12 @@ public class GameData
         List<bool> levelClearList = new List<bool>();
         List<int> idChalangeList = new List<int>();
         List<bool> chalangeClearList = new List<bool>();
+        List<string> chalangeDescriptionList = new List<string>();
         List<float> miniGameScoreList = new List<float>();
         List<int> idMiniGameList = new List<int>();
         List<bool> openMiniGameList = new List<bool>();
+        List<string> miniGameDescriptionList = new List<string>();
+
         foreach (Stage stage in stages)
         {
             levelList.Add(stage.Level);
@@ -30,6 +35,8 @@ public class GameData
             {
                 idChalangeList.Add(chalange.IdChalange);
                 chalangeClearList.Add(chalange.Clear);
+                chalangeDescriptionList.Add(chalange.NameChalange);
+                Debug.Log("nama chalange = " + chalange.NameChalange);
             }
         }
         foreach(MiniGame miniGame in miniGames)
@@ -37,13 +44,16 @@ public class GameData
             miniGameScoreList.Add(miniGame.Highscore);
             idMiniGameList.Add(miniGame.Id);
             openMiniGameList.Add(miniGame.Opened);
+            miniGameDescriptionList.Add(miniGame.Description);
         };
         level = levelList.ToArray();
         levelClear = levelClearList.ToArray();
         idChalange = idChalangeList.ToArray();
         chalangeClear = chalangeClearList.ToArray();
+        chalangeDescription = chalangeDescriptionList.ToArray();
         miniGameScore = miniGameScoreList.ToArray();
         idMiniGame = idMiniGameList.ToArray();
         openMiniGame = openMiniGameList.ToArray();
+        miniGameDescription = miniGameDescriptionList.ToArray();
     }
 }
--- Sripsi history/Assets/Scripts/MiniGame.cs
+++ Sripsi history/Assets/Scripts/MiniGame.cs
@@ -22,6 +22,14 @@ public class MiniGame
         Description = desc[id-1];
     }
 
+    public MiniGame(int id, string description, float highscore = 0, bool opened = false)
+    {
+        Id = id;
+        Highscore = highscore;
+        Opened = opened;
+        Description = description;
+    }
+
     string[] desc = new string[] {
         "Pangeran Diponegoro harus bertahan hidup dari serangan musuh. Semakin lama nilai semakin tinggi.",
         "Pangeran Diponegoro mengejar musuh - musuhnya menggunakan kuda. Semakin lama nilai semakin tinggi."
